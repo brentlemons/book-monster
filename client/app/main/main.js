852487762e701c920772693e158fbd9829ecfa27
Grant Lemons <grant_lemons@yahoo.com>
2015-03-01 14:25:17 -0600

    starting development of book-monster

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('bookMonsterApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
